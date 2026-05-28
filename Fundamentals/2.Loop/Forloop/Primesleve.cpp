4f28ba38ede711f9956cb7766959cef279978be9
Ashutosh-03547 <pathyashutosh65@gmail.com>
2026-05-28 20:59:56 +0530

    Solved prime sleve  problems

--- Fundamentals/2.Loop/Forloop/Primesleve.cpp
+++ Fundamentals/2.Loop/Forloop/Primesleve.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
+#include <vector>
 using namespace std;
  int main(){
-    int i,j,num;
-    cout<<"Enter the number:";
-    cin>>num;
-    for(i=2;i<=num;i++){
-        if(i!=2){
-            if(i%2!=0){
-                cout<<i<<endl;
-             }
+     int p,i,j,num;
+     cout<<"enter the number:";
+     cin>>num;
+     vector<bool> sleve(num+1,true);//vector is used to indicate the numeric value and bool is used to indicate the boolean value 
+     sleve[0]=sleve[1]=false;// excluding 0,1 
+     for(p=2;p*p<=num;p++){
+        if(sleve[p]){
+            for(j=p*p;j<=num;j+=p){
+               sleve[j]=false;
+            }
         }
-        else{
+     }
+
+     for(i=2;i<=num;i++){
+        if(sleve[i]){
             cout<<i<<endl;
         }
-        
-    }
-
+     }
 
     return 0;
+    
  }
