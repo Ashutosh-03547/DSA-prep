5066ede57b0aa8ddb5927ea17755b637c2baee4a
Ashutosh-03547 <pathyashutosh65@gmail.com>
2026-06-12 21:53:07 +0530

    update ncr problems

--- Fundamentals/3.Functions/ncrproblem.cpp
+++ Fundamentals/3.Functions/ncrproblem.cpp
@@ -1,34 +1,12 @@
 #include<iostream>
 using namespace std;
 
-long long factorial(int value){
-  long long fact = 1;
-  for(int i = 1; i <= value; i++){
-    fact *= i;
+double factorial(int num){
+  for(int i=1;i<=num;i++){
+      num=num*i;
   }
-  return fact;
+  return num;
 }
-
-void ncr(int n, int r){
-  if(r > n){
-    cout << 0 << endl;
-    return;
-  }
-
-  int c = n - r;
-  long long nFactorial = factorial(n);
-  long long rFactorial = factorial(r);
-  long long cFactorial = factorial(c);
-
-  cout << cFactorial << endl;
-
-  long long ncrValue = nFactorial / (rFactorial * cFactorial);
-  cout << ncrValue << endl;
-}
-
-int main(){
-  int n, r;
-  cin >> n >> r;
-  ncr(n, r);
-  return 0;
+double ncr(){
+    
 }
